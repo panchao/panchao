5ec56ec64817bf7f254b9563cb19b80a2d0c0ab4
chuanzongliu <liuchzong@qq.com>
2015-07-01 00:55:52 +0800

    增加面包屑导航

--- aoxiuProject/aoxiuPhoto/src/main/webapp/js/helper/bread-crumb/bread-crumb.js
+++ aoxiuProject/aoxiuPhoto/src/main/webapp/js/helper/bread-crumb/bread-crumb.js
@@ -1,5 +1,117 @@
 // require jquery, ejs
+;(function (window) {
 
+  function factory($, EJS) {
+
+    function BreadCrumb(customerSettings) {
+      this._init(customerSettings) || this._event();
+    }
+    
+    // 私有函数:
+    // _init
+    // _event
+    // _update
+
+    // 公有函数
+    // append
+    // $current
+    BreadCrumb.prototype = {
+      constructor: BreadCrumb,
+      
+      // 初始化界面
+      _init: function (customerSettings) {
+        var settings = $.extend({}, BreadCrumb.defaults, customerSettings);
+        this.$target = $(settings.hook);
+
+        if (this.$target.length === 0 || !settings.template) {
+          throw new TypeError('hook or template is empty');
+        }
+        // 初始化一次
+        this.ajax = settings.ajax;
+        this.callback = settings.callback;
+        this.template = new EJS({ url: settings.template });
+        this.config = {
+          ajax: this.ajax,
+          dirs: settings.dirs
+        };
+        if (this.$target.data('breadCrumbStatus') === 'initialized') {
+          return true;
+        }
+
+        // only process the first one
+        this._update();
+        
+        this.$target.data('breadCrumbStatus', 'initialized');
+        return false;
+      },
+      
+      // 响应事件
+      _event: function () {
+        var self = this;
+        if (self.ajax) {
+          self.$target.on('click', 'a', function (event) {
+            event.preventDefault();
+            $.getJSON(this.href, self.callback);
+
+            self.config.dirs.splice($(this).parent().index() + 1);
+            self._update();
+          });
+        }
+      },
+
+      // 更新 breadcrumb
+      _update: function () {
+        this.template.update(this.$target[0], this.config);
+      },
+
+      // 添加 breadcrumb
+      append: function (url, text) {
+        this.config.dirs.push({url: url, text: text});
+        this._update();
+      },
+      // 返回当前 目录的jquery dom
+      $current: function () {
+        return this.$target.find('.active');
+      }
+    };
+    
+    // @hook html hook
+    BreadCrumb.defaults = {
+      // hook : '#bread-crumb',
+      /*dirs: [{
+        text: '主目录',
+        href: '/items'
+      }, {
+        text: '一级目录',
+        href: '/items/1'
+      }, {
+        text: '二级目录',
+        href: '/items/2'
+      }],*/
+      callback: function (msg) {
+        console.log(msg);
+      },
+      template: '/js/helper/bread-crumb/template/bread-crumb.ejs',
+      ajax: true
+    };
+
+    return BreadCrumb;
+  }
+
+  // adapt to requirejs
+  if (window.define) {
+    define(['jquery', 'ejs'], function ($, EJS) {
+      return factory($, EJS);
+    });
+  } 
+  else {
+    legend.helper.BreadCrumb = factory($, EJS);
+  }
+}(window));
+
+
+
+/*
 (function ($, EJS) {
   'use strict';
   
@@ -11,6 +123,7 @@
   // _init
   // _event
   // _update
+
   // 公有函数
   // append
   // $current
@@ -77,16 +190,16 @@
   // @hook html hook
   BreadCrumb.defaults = {
     // hook : '#bread-crumb',
-    /*dirs: [{
-      text: '主目录',
-      href: '/items'
-    }, {
-      text: '一级目录',
-      href: '/items/1'
-    }, {
-      text: '二级目录',
-      href: '/items/2'
-    }],*/
+    // dirs: [{
+    //   text: '主目录',
+    //   href: '/items'
+    // }, {
+    //   text: '一级目录',
+    //   href: '/items/1'
+    // }, {
+    //   text: '二级目录',
+    //   href: '/items/2'
+    // }],
     callback: function (msg) {
       console.log(msg);
     },
@@ -94,6 +207,6 @@
     ajax: true
   };
   
-  // make BreadCrumb visible
+  // make BreadCrumb public to window
   legend.helper.BreadCrumb = BreadCrumb;
-}(jQuery, EJS));
+}(window.jQuery, window.EJS));*/
